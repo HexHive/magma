df54c0f77fb78f98da9c2f5ee560da0675853fb7
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-02-03 19:51:55 +0100

    Improved captain run.sh script
    - Added option to mount shared volume as tmpfs (to reduce I/O overhead)
    - Added option to archive campaign workdirs
    - Added config.yaml to selectively launch fuzz campaigns

--- tools/captain/run.sh
+++ tools/captain/run.sh
@@ -3,16 +3,24 @@ set -e
 
 ##
 # Pre-requirements:
+# + $1: path to config.yaml
 # - env WORKDIR: path to directory where shared volumes will be created
 # - env REPEAT: number of campaigns to run per program (per fuzzer)
 # + env WORKERS: number of worker threads (default: CPU cores)
 # + env TIMEOUT: time to run each campaign (default: 1m)
 # + env POLL: time (in seconds) between polls (default: 5s)
+# + env MAGMA_CACHE_ON_DISK: if defined, the cache workdir is mounted on disk
+#       instead of in-memory (default: undefined)
+# + env MAGMA_NO_ARCHIVE: if defined, campaign workdirs will not be tarballed
+#       (default: undefined)
 ##
 
 if [ -z $WORKDIR ] || [ -z $REPEAT ]; then
     echo '$WORKDIR and $REPEAT must be specified as environment variables.'
     exit 1
+fi
+if [ -z $1 ]; then
+    set -- "config.yaml"
 fi
 MAGMA=${MAGMA:-"$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" >/dev/null 2>&1 \
     && pwd)"}
@@ -38,12 +46,20 @@ start_campaign()
     export PROGRAM="$3"
     export ITERATION="$4"
     export AFFINITY="$5"
-    export SHARED="$WORKDIR/$FUZZER/$TARGET/$PROGRAM/$ITERATION"
+    export SHARED="$WORKDIR/cache/$FUZZER/$TARGET/$PROGRAM/$ITERATION"
     echo "Starting: $FUZZER/$TARGET/$PROGRAM/$ITERATION on CPU $AFFINITY"
     mkdir -p "$SHARED" && chmod 777 "$SHARED"
-    sem --id "magma_cpu_$AFFINITY" -j 1 \
-        tools/captain/start.sh \
+    sem --id "magma_cpu_$AFFINITY" --fg -j 1 \
+        "$MAGMA/tools/captain/start.sh" \
         1>/dev/null 2>&1
+    AR="$WORKDIR/ar/$FUZZER/$TARGET/$PROGRAM"
+    mkdir -p "$AR"
+    if [ -z $MAGMA_NO_ARCHIVE ]; then
+        tar -cf "${AR}/${ITERATION}.tar" "$SHARED"
+    else
+        mv "$SHARED" "$AR"
+    fi
+    rm -rf "$SHARED"
 }
 export -f start_campaign
 
@@ -66,19 +82,31 @@ get_free_cpu()
     done
 }
 
-cd "$MAGMA"
-for FUZZER in fuzzers/*; do
-    export FUZZER="${FUZZER##*/}"
-    for TARGET in targets/*; do
-    # for TARGET in openssl; do
-        export TARGET="${TARGET##*/}"
+# clear any stuck semaphores
+rm -rf ~/.parallel/semaphores/id-magma*
+
+# set up a RAM-backed fs for fast processing of canaries and crashes
+mkdir -p "$WORKDIR/cache"
+mkdir -p "$WORKDIR/ar"
+if [ -z $MAGMA_CACHE_ON_DISK ]; then
+    echo "Obtaining sudo permissions to mount tmpfs"
+    sudo mount -t tmpfs -o size=200g,uid=$(id -u $USER),gid=$(id -g $USER) \
+        tmpfs "$WORKDIR/cache"
+fi
+
+echo "$(yq r --printMode p "$1" '*')" | \
+while read FUZZER; do
+    export FUZZER
+    echo "$(yq r "$1" $FUZZER.'**')" | \
+    while read TARGET; do
+        export TARGET
         # build the magma/fuzzer/target Docker image
         IMG_NAME="magma/$FUZZER/$TARGET"
         echo "Building $IMG_NAME"
-        tools/captain/build.sh 1>/dev/null 2>&1
+        $MAGMA/tools/captain/build.sh 1>/dev/null 2>&1
 
         # start the multiple fuzzer campaigns
-        yq read "targets/$TARGET/config.yaml" programs | \
+        yq read "$MAGMA/targets/$TARGET/config.yaml" programs | \
         while read PROGRAM; do
             PROGRAM="$(echo "$PROGRAM" | cut -c 3-)"
             for ((i=0; i<$REPEAT; i++)); do
@@ -92,3 +120,8 @@ for FUZZER in fuzzers/*; do
 done
 
 sem --id "magma" --wait
+
+if [ -z $MAGMA_CACHE_ON_DISK ]; then
+    echo "Obtaining sudo permissions to umount tmpfs"
+    sudo umount "$WORKDIR/cache"
+fi
