b1c1cf11bde06fb4749a7cb5671b763949e6ccef
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-02-13 12:09:44 +0100

    Added remark explaining the canary data structure

--- magma/src/common.h
+++ magma/src/common.h
@@ -19,6 +19,8 @@ typedef union {
     canary_storage_t raw[CANARY_TYPE_COUNT];
 } canary_t;
 
+// The `2` in the denominator is for splitting the region between producer and
+// consumer buffers. It has nothing to do with CANARY_TYPE_COUNT.
 typedef canary_t data_t[(SIZE-sizeof(max_align_t))/sizeof(canary_t)/2];
 typedef struct {
     bool consumed;
