9fb45be71f1d1b79d99c4166543a1c5458a67e34
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-04-21 12:14:13 +0200

    Added php build scripts and patches

--- targets/php/build.sh
+++ targets/php/build.sh
@@ -13,18 +13,40 @@ if [ ! -d "$TARGET/repo" ]; then
     exit 1
 fi
 
-#build the php library
 cd "$TARGET/repo"
-./configure \
+export ONIG_CFLAGS="-I$PWD/oniguruma/src"
+export ONIG_LIBS="-L$PWD/oniguruma/src/.libs -l:libonig.a"
+
+# PHP's zend_function union is incompatible with the object-size sanitizer
+export CFLAGS="$CFLAGS -fno-sanitize=object-size"
+export CXXFLAGS="$CXXFLAGS -fno-sanitize=object-size"
+
+#build the php library
+./buildconf
+LIB_FUZZING_ENGINE="-Wall" ./configure \
+    --disable-all \
+    --enable-option-checking=fatal \
     --enable-fuzzer \
-    --with-pic \
-    --enable-debug-assertions \
+    --enable-json \
     --enable-exif \
+    --enable-mbstring \
+    --without-pcre-jit \
+    --disable-phpdbg \
+    --disable-cgi \
+    --with-pic
+
 make -j$(nproc) clean
-make -j$(nproc) 
 
+# build oniguruma and link statically
+pushd oniguruma
+autoreconf -vfi
+./configure
+make -j$(nproc)
+popd
 
-#Copy the generated seeds to the artifacts location
-cp sapi/fuzzer/php-fuzz-exif $OUT/$
+make -j$(nproc)
 
-
+FUZZERS="php-fuzz-json php-fuzz-exif php-fuzz-mbstring php-fuzz-unserialize php-fuzz-parser"
+for fuzzerName in $FUZZERS; do
+	cp sapi/fuzzer/$fuzzerName "$OUT/"
+done
