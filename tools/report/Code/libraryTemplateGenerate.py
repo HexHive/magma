61ff84248aa94e3d7ade825f14c1c114c2be00d4
cjordan7 <cosmehbvjordan@gmail.com>
2020-05-13 15:07:22 +0200

    Changes

--- tools/report/Code/libraryTemplateGenerate.py
+++ tools/report/Code/libraryTemplateGenerate.py
@@ -2,26 +2,26 @@ from Code.Render import Render
 
 
 class LibraryTemplate(Render):
-    LIBRARY_TEMPLATE = "library_template.html"
     '''
         A class to represent a library
     '''
+
+    LIBRARY_TEMPLATE = "library_template.html"
+
     def __init__(self, path):
         '''
-            Parameters
-            ----------
-            path(Path):
-                of class Path, used to have all the useful paths
+        Parameters
+        ----------
+        path(Path):
+            of class Path, used to have all the useful paths
+        '''
 
-            _file_:
-                The file to get the directory from
-            '''
         self.path = path
         self.template_dir = path.template_dir
         self.output_dir = path.output_dir
         self.tables_dir = path.tables_dir
         self.plot_dir = path.plot_dir
-    #TODO reaplce description which is only meant for fuzzers
+
     def render(self, file_name, output_file_name, description):
         template = self.path.get_template(file_name)
 
--- tools/report/Code/mainPageTemplateGenerate.py
+++ tools/report/Code/mainPageTemplateGenerate.py
@@ -1,32 +1,37 @@
-from jinja2 import Environment, FileSystemLoader, Template
 import os
 
-from Code.Render import Render
+from Render import Render
 
 
 class MainPageTemplate(Render):
     '''
        A class to represent a the main report page
     '''
-    def __init__(self, path):
+
+    def __init__(self, path, fuzzers, libraries):
         '''
         Parameters
         ----------
         path(Path):
             of class Path, used to have all the useful paths
 
-        _file_:
-            The file to get the directory from
+        fuzzers:
+
+        libraries:
         '''
-        self.path = path
+
+        self.fuzzers = fuzzers
+        self.libraries = libraries
+        super(MainPageTemplate, self).__init__(path)
+
         # Set paths for templates, output and images
         self.template_dir = path.template_dir
         self.output_dir = path.output_dir
         self.tables_dir = path.tables_dir
         self.plot_dir = path.plot_dir
 
-    def render(self, file_name, output_file_name, description):
-        '''
+    def render(self, file_name, output_file_name):
+        """
         Generate (write to html file) and render reports (html, bugs reports,
         tables,...)
 
@@ -34,31 +39,24 @@ class MainPageTemplate(Render):
         ----------
         file_name (string):
             The file to get the directory from
-
         output_file_name (string):
             The name of the html file to write to. (example: "afl")
-
-        description (FuzzerDescription):
-            The corresponding description
-        '''
-
-        # TODO fix the inheritance for description whcih in libraryGenerate and MainPageGenerate makes no sense
+        """
 
         template = self.path.get_template(file_name)
+        TARGET_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../../targets"))
+        FUZZER_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../../fuzzers"))
+        fuzzer_list = [f.name for f in os.scandir(FUZZER_DIR) if f.is_dir()]
+        target_list = [f.name for f in os.scandir(TARGET_DIR) if f.is_dir()]
+        target_number_of_bug_list = []
 
-        '''
-        target_list (list of string) ;
-            The target libraries name
-        
-        total_bugs (int) :
-            total number of implemented bugs
-            
-        fuzzer_list (list of string) :
-            The benchmarked fuzzers
-        '''
-        target_list = []
-        total_bugs = 0
-        fuzzer_list = []
+        for target in target_list:
+            patch_path = os.path.join(TARGET_DIR, target, "patches/bugs")
+            number_of_bugs = len([name for name in os.listdir(patch_path)])
+            target_number_of_bug_list.append(number_of_bugs)
+        total_bugs = sum(target_number_of_bug_list)
+        target_list = zip(target_list, target_number_of_bug_list)
+
         # TODO Get all the above information from the json passed as argument
         rendering = template.render(target_list=target_list, total_bugs=total_bugs, fuzzer_list=fuzzer_list)
 
@@ -83,5 +81,4 @@ class MainPageTemplate(Render):
         total_bugs = sum(target_number_of_bug_list)
         target_list = zip(target_list,target_number_of_bug_list)
         template.stream(target_list=target_list,total_bugs=total_bugs,fuzzer_list=fuzzer_list).dump('report.html')
-
     '''
