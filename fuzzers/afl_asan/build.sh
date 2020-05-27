5f8fb44f3c486889fe2049dbb76024f379a44887
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-05-27 20:08:36 +0200

    Added an AFL configuration with ASAN

---
+++ fuzzers/afl_asan/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+##
+
+if [ ! -d "$FUZZER/repo" ]; then
+    echo "fetch.sh must be executed first."
+    exit 1
+fi
+
+cd "$FUZZER/repo"
+CC=clang make -j $(nproc)
+CC=clang make -j $(nproc) -C llvm_mode
+
+# compile afl_driver.cpp
+"./afl-clang-fast++" $CXXFLAGS -fsanitize=address -std=c++11 -c "afl_driver.cpp" -fPIC -o "$OUT/afl_driver.o"
---
+++ fuzzers/afl_asan/preinstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+apt-get update && \
+    apt-get install -y make build-essential clang-9 git wget
+
+update-alternatives \
+  --install /usr/lib/llvm              llvm             /usr/lib/llvm-9  20 \
+  --slave   /usr/bin/llvm-config       llvm-config      /usr/bin/llvm-config-9  \
+    --slave   /usr/bin/llvm-ar           llvm-ar          /usr/bin/llvm-ar-9 \
+    --slave   /usr/bin/llvm-as           llvm-as          /usr/bin/llvm-as-9 \
+    --slave   /usr/bin/llvm-bcanalyzer   llvm-bcanalyzer  /usr/bin/llvm-bcanalyzer-9 \
+    --slave   /usr/bin/llvm-c-test       llvm-c-test      /usr/bin/llvm-c-test-9 \
+    --slave   /usr/bin/llvm-cov          llvm-cov         /usr/bin/llvm-cov-9 \
+    --slave   /usr/bin/llvm-diff         llvm-diff        /usr/bin/llvm-diff-9 \
+    --slave   /usr/bin/llvm-dis          llvm-dis         /usr/bin/llvm-dis-9 \
+    --slave   /usr/bin/llvm-dwarfdump    llvm-dwarfdump   /usr/bin/llvm-dwarfdump-9 \
+    --slave   /usr/bin/llvm-extract      llvm-extract     /usr/bin/llvm-extract-9 \
+    --slave   /usr/bin/llvm-link         llvm-link        /usr/bin/llvm-link-9 \
+    --slave   /usr/bin/llvm-mc           llvm-mc          /usr/bin/llvm-mc-9 \
+    --slave   /usr/bin/llvm-nm           llvm-nm          /usr/bin/llvm-nm-9 \
+    --slave   /usr/bin/llvm-objdump      llvm-objdump     /usr/bin/llvm-objdump-9 \
+    --slave   /usr/bin/llvm-ranlib       llvm-ranlib      /usr/bin/llvm-ranlib-9 \
+    --slave   /usr/bin/llvm-readobj      llvm-readobj     /usr/bin/llvm-readobj-9 \
+    --slave   /usr/bin/llvm-rtdyld       llvm-rtdyld      /usr/bin/llvm-rtdyld-9 \
+    --slave   /usr/bin/llvm-size         llvm-size        /usr/bin/llvm-size-9 \
+    --slave   /usr/bin/llvm-stress       llvm-stress      /usr/bin/llvm-stress-9 \
+    --slave   /usr/bin/llvm-symbolizer   llvm-symbolizer  /usr/bin/llvm-symbolizer-9 \
+    --slave   /usr/bin/llvm-tblgen       llvm-tblgen      /usr/bin/llvm-tblgen-9
+
+update-alternatives \
+  --install /usr/bin/clang                 clang                  /usr/bin/clang-9     20 \
+  --slave   /usr/bin/clang++               clang++                /usr/bin/clang++-9 \
+  --slave   /usr/bin/clang-cpp             clang-cpp              /usr/bin/clang-cpp-9
