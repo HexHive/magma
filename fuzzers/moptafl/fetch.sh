41547de150cb3448222fc07149840796f1c63b77
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-05-31 10:38:52 +0200

    Update MOpt-AFL fetch script to match upstream

--- fuzzers/moptafl/fetch.sh
+++ fuzzers/moptafl/fetch.sh
@@ -7,7 +7,7 @@ set -e
 ##
 
 git clone --depth 1 https://github.com/puppet-meteor/MOpt-AFL.git "$FUZZER/repo"
-mv "$FUZZER/repo/MOpt-AFL V1.0"/* "$FUZZER/repo"
+mv "$FUZZER/repo/MOpt"/* "$FUZZER/repo"
 #wget -O "$FUZZER/repo/afl_driver.cpp" \
 #    "https://cs.chromium.org/codesearch/f/chromium/src/third_party/libFuzzer/src/afl/afl_driver.cpp"
 cp "$FUZZER/src/afl_driver.cpp" "$FUZZER/repo/afl_driver.cpp"
