c9351c81bf4be1230370d05ae6ed72c5aeddafd6
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-06-17 17:38:44 +0200

    Fixed wrong MOpt-AFL fetch script

--- fuzzers/moptafl_asan/fetch.sh
+++ fuzzers/moptafl_asan/fetch.sh
@@ -6,7 +6,8 @@ set -e
 # - env FUZZER: path to fuzzer work dir
 ##
 
-git clone --depth 1 https://github.com/google/AFL.git "$FUZZER/repo"
+git clone --depth 1 https://github.com/puppet-meteor/MOpt-AFL.git "$FUZZER/repo"
+mv "$FUZZER/repo/MOpt"/* "$FUZZER/repo"
 #wget -O "$FUZZER/repo/afl_driver.cpp" \
 #    "https://cs.chromium.org/codesearch/f/chromium/src/third_party/libFuzzer/src/afl/afl_driver.cpp"
 cp "$FUZZER/src/afl_driver.cpp" "$FUZZER/repo/afl_driver.cpp"
