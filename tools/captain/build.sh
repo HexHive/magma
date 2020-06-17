8da3f0a847e1de77435fe1ab619f3a20ccde61f3
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-06-17 14:44:49 +0200

    Support incremental changes in image build process

--- tools/captain/build.sh
+++ tools/captain/build.sh
@@ -6,9 +6,6 @@
 # - env TARGET: target name (from targets/)
 # + env MAGMA: path to magma root (default: ../../)
 # + env FORCE: if set, force build even if image exists (default: 0)
-# + env NO_CANARIES: if set, build the benchmark without canaries (default:
-#       unset)
-# + env FIXES: if set, build the benchmark without bugs (default: unset)
 # + env ISAN: if set, build the benchmark with ISAN/fatal canaries (default:
 #       unset)
 # + env HARDEN: if set, build the benchmark with hardened canaries (default:
@@ -24,12 +21,20 @@ MAGMA=${MAGMA:-"$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" >/dev/null 2>&1 \
     && pwd)"}
 source "$MAGMA/tools/captain/common.sh"
 
-if [ -z $NO_CANARIES ]; then
-    canaries_flag="--build-arg canaries=1"
-fi
-if [ ! -z $FIXES ]; then
-    fixes_flag="--build-arg fixes=1"
-fi
+CANARY_MODE=${CANARY_MODE:-1}
+
+case $CANARY_MODE in
+1)
+    mode_flag="--build-arg canaries=1"
+    ;;
+2)
+    mode_flag=""
+    ;;
+3)
+    mode_flag="--build-arg fixes=1"
+    ;;
+esac
+
 if [ ! -z $ISAN ]; then
     isan_flag="--build-arg isan=1"
 fi
@@ -44,7 +49,7 @@ if [ -z $(docker image ls -q "$IMG_NAME") ] || [ ! -z $FORCE ]; then
         --build-arg target_name="$TARGET" \
         --build-arg USER_ID=$(id -u $USER) \
         --build-arg GROUP_ID=$(id -g $USER) \
-        $canaries_flag $fixes_flag $isan_flag $harden_flag \
+        $mode_flag $isan_flag $harden_flag \
         -f "$MAGMA/docker/Dockerfile" "$MAGMA"
     set +x
 fi
