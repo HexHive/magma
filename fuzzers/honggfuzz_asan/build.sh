50f1b21ecd745dab47932b3040ce5b5c17fff808
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-06-19 01:44:15 +0200

    Added a honggfuzz_asan fuzzer configuration

---
+++ fuzzers/honggfuzz_asan/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+##
+
+if [ ! -d "$FUZZER/repo" ]; then
+    echo "fetch.sh must be executed first."
+    exit 1
+fi
+
+cd "$FUZZER/repo"
+CC=clang CXX=clang++ make -j $(nproc)
---
+++ fuzzers/honggfuzz_asan/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+# - env TARGET: path to target work dir
+# - env OUT: path to directory where artifacts are stored
+# - env SHARED: path to directory shared with host (to store results)
+# - env PROGRAM: name of program to run (should be found in $OUT)
+# - env ARGS: extra arguments to pass to the program
+##
+
+mkdir -p "$SHARED/findings" "$SHARED/output"
+
+# replace AFL-style input file parameter with honggfuzz-style one
+ARGS="${ARGS/@@/___FILE___}"
+
+"$FUZZER/repo/honggfuzz" -n 1 -z --input "$TARGET/corpus/$PROGRAM" \
+    --output "$SHARED/output" --workspace "$SHARED/findings" \
+    -- "$OUT/$PROGRAM" $ARGS 2>&1
