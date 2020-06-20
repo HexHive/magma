b4c284c59d0e815446c5011248b153a44a860afb
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-06-20 16:30:35 +0200

    Anchored honggfuzz_asan reference

--- fuzzers/honggfuzz_asan/fetch.sh
+++ fuzzers/honggfuzz_asan/fetch.sh
@@ -6,4 +6,6 @@ set -e
 # - env FUZZER: path to fuzzer work dir
 ##
 
-git clone --depth 1 https://github.com/google/honggfuzz.git "$FUZZER/repo"
+git clone --no-checkout https://github.com/google/honggfuzz.git \
+    "$FUZZER/repo"
+git -C "$FUZZER/repo" checkout 5fa5d0a1aecdb422091a3eb723e2aae80c122209
