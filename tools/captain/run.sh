313719608739fb2768ed846db04be067c2f67153
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-06-22 16:34:57 +0200

    Fixed data race with NO_CACHE_ON_DISK

--- tools/captain/run.sh
+++ tools/captain/run.sh
@@ -98,10 +98,11 @@ start_campaign()
     if [ -z $NO_ARCHIVE ]; then
         # only one tar job runs at a time, to prevent out-of-storage errors
         sem --id "magma_tar" --fg -j 1 \
-          tar -cf "${CAMPAIGN_ARDIR}/${CID}/${CID}.tar" -C "$SHARED" . &>/dev/null && \
-        rm -rf "$SHARED"
+          tar -cf "${CAMPAIGN_ARDIR}/${CID}.tar" -C "$SHARED" . &>/dev/null && \
+        rm -rf "$SHARED" "${CAMPAIGN_ARDIR}/${CID}"
     else
-        rm -rf "${CAMPAIGN_ARDIR}/${CID}" && mv "$SHARED" "${CAMPAIGN_ARDIR}/${CID}"
+        # overwrites empty $CID directory with the $SHARED directory
+        mv -T "$SHARED" "${CAMPAIGN_ARDIR}/${CID}"
     fi
 }
 export -f start_campaign
