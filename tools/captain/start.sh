e94ce7c45302a1fbb97edcfb90127e9c3e26b726
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-06-23 21:12:45 +0200

    Added option to run custom entry point in start.sh

--- tools/captain/start.sh
+++ tools/captain/start.sh
@@ -6,20 +6,25 @@
 # - env TARGET: target name (from targets/)
 # - env PROGRAM: program name (name of binary artifact from $TARGET/build.sh)
 # - env ARGS: program launch arguments
-# - env SHARED: path to host-local volume where fuzzer findings are saved
 # - env POLL: time (in seconds) between polls
 # - env TIMEOUT: time to run the campaign
+# + env SHARED: path to host-local volume where fuzzer findings are saved
+#       (default: no shared volume)
 # + env AFFINITY: the CPU to bind the container to (default: no affinity)
+# + env ENTRYPOINT: a custom entry point to launch in the container (default:
+#       $MAGMA/run.sh)
 ##
 
 cleanup() {
-    docker rm -f $container_id 1>/dev/null 2>&1
+    if [ ! -t 1 ]; then
+        docker rm -f $container_id &> /dev/null
+    fi
 }
 
 trap cleanup EXIT
 
-if [ -z $FUZZER ] || [ -z $TARGET ] || [ -z $PROGRAM ] || [ -z $SHARED ]; then
-    echo '$FUZZER, $TARGET, $PROGRAM, and $SHARED must be specified as' \
+if [ -z $FUZZER ] || [ -z $TARGET ] || [ -z $PROGRAM ]; then
+    echo '$FUZZER, $TARGET, and $PROGRAM must be specified as' \
          'environment variables.'
     exit 1
 fi
@@ -35,14 +40,30 @@ if [ ! -z $AFFINITY ]; then
     flag_aff="--cpuset-cpus=$AFFINITY --env=AFFINITY=$AFFINITY"
 fi
 
-container_id=$(
-docker run -dt --volume=`realpath "$SHARED"`:/magma_shared \
+if [ ! -z "$ENTRYPOINT" ]; then
+    flag_ep="--entrypoint=$ENTRYPOINT"
+fi
+
+if [ ! -z "$SHARED" ]; then
+    SHARED="$(realpath "$SHARED")"
+    flag_volume="--volume=$SHARED:/magma_shared"
+fi
+
+if [ -t 1 ]; then
+    docker run -it $flag_volume \
         --cap-add=SYS_PTRACE --env=PROGRAM="$PROGRAM" --env=ARGS="$ARGS" \
-        --env=POLL="$POLL" --env=TIMEOUT="$TIMEOUT" $flag_aff \
+        --env=POLL="$POLL" --env=TIMEOUT="$TIMEOUT" $flag_aff $flag_ep \
         "$IMG_NAME"
-)
-container_id=$(cut -c-12 <<< $container_id)
-echo_time "Container for $FUZZER/$TARGET/$PROGRAM/$CID started in $container_id"
-docker logs -f "$container_id" &
-exit_code=$(docker wait $container_id)
-exit $exit_code
+else
+    container_id=$(
+    docker run -dt $flag_volume \
+        --cap-add=SYS_PTRACE --env=PROGRAM="$PROGRAM" --env=ARGS="$ARGS" \
+        --env=POLL="$POLL" --env=TIMEOUT="$TIMEOUT" $flag_aff $flag_ep \
+        "$IMG_NAME"
+    )
+    container_id=$(cut -c-12 <<< $container_id)
+    echo_time "Container for $FUZZER/$TARGET/$PROGRAM/$CID started in $container_id"
+    docker logs -f "$container_id" &
+    exit_code=$(docker wait $container_id)
+    exit $exit_code
+fi
