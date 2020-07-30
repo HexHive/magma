6d6aba53aba76c4e6d0f5ee17404b1b11c040e88
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-07-30 12:00:16 +0200

    Fixed broken build after AFL++ improvements
    # Conflicts:
    #	fuzzers/aflplusplus/fetch.sh

--- fuzzers/aflplusplus/fetch.sh
+++ fuzzers/aflplusplus/fetch.sh
@@ -9,3 +9,15 @@ set -e
 git clone --no-checkout https://github.com/AFLplusplus/AFLplusplus.git \
     "$FUZZER/repo"
 git -C "$FUZZER/repo" checkout 4a51cb71fb8785325dedac693cdea4648f6e5279
+
+# Fix: CMake-based build systems fail with duplicate or undefined references
+sed -i '{s/^int main/__attribute__((weak)) &/}' $FUZZER/repo/examples/aflpp_driver/
+sed -i '{s/^int LLVMFuzzerTestOneInput/__attribute__((weak)) &/}' $FUZZER/repo/examples/aflpp_driver/aflpp_driver.cpp
+cat >> $FUZZER/repo/examples/aflpp_driver/aflpp_driver.cpp << EOF
+extern "C" __attribute__((weak))
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{
+  assert(false && "LLVMFuzzerTestOneInput should not be implemented in afl_driver");
+  return 0;
+}
+EOF
--- fuzzers/aflplusplus/run.sh
+++ fuzzers/aflplusplus/run.sh
@@ -12,7 +12,7 @@
 
 mkdir -p "$SHARED/findings"
 
-flag_cmplog="-m none -c \"$OUT/cmplog/$PROGRAM\""
+flag_cmplog=(-m none -c "$OUT/cmplog/$PROGRAM")
 
 export AFL_SKIP_CPUFREQ=1
 export AFL_NO_AFFINITY=1
@@ -20,5 +20,5 @@ export AFL_NO_UI=1
 export AFL_MAP_SIZE=256000
 
 "$FUZZER/repo/afl-fuzz" -i "$TARGET/corpus/$PROGRAM" -o "$SHARED/findings" \
-    $flag_cmplog -d \
+    "${flag_cmplog[@]}" -d \
     -- "$OUT/afl/$PROGRAM" $ARGS 2>&1
