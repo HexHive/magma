2f450ec45afa9e7b28761c669389d29bfe47b99d
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-08-10 19:42:40 +0200

    Weak-ened AFL driver symbols for config/cmake

--- fuzzers/aflplusplus/fetch.sh
+++ fuzzers/aflplusplus/fetch.sh
@@ -10,7 +10,7 @@ git clone --no-checkout https://github.com/AFLplusplus/AFLplusplus.git \
     "$FUZZER/repo"
 git -C "$FUZZER/repo" checkout 986af28df27016813abdfdde8bdedda1f571703c
 
-# Fix: CMake-based build systems fail with duplicate or undefined references
+# Fix: CMake-based build systems fail with duplicate (of main) or undefined references (of LLVMFuzzerTestOneInput)
 sed -i '{s/^int main/__attribute__((weak)) &/}' $FUZZER/repo/examples/aflpp_driver/aflpp_driver.c
 sed -i '{s/^int LLVMFuzzerTestOneInput/__attribute__((weak)) &/}' $FUZZER/repo/examples/aflpp_driver/aflpp_driver.c
 cat >> $FUZZER/repo/examples/aflpp_driver/aflpp_driver.c << EOF
--- fuzzers/aflplusplus_lto/fetch.sh
+++ fuzzers/aflplusplus_lto/fetch.sh
@@ -6,6 +6,18 @@ set -e
 # - env FUZZER: path to fuzzer work dir
 ##
 
-git clone https://github.com/AFLplusplus/AFLplusplus "$FUZZER/repo"
-cd "$FUZZER/repo" || exit 1
-git checkout dev
+git clone --no-checkout https://github.com/AFLplusplus/AFLplusplus.git \
+    "$FUZZER/repo"
+git -C "$FUZZER/repo" checkout 986af28df27016813abdfdde8bdedda1f571703c
+
+# Fix: CMake-based build systems fail with duplicate (of main) or undefined references (of LLVMFuzzerTestOneInput)
+sed -i '{s/^int main/__attribute__((weak)) &/}' $FUZZER/repo/examples/aflpp_driver/aflpp_driver.c
+sed -i '{s/^int LLVMFuzzerTestOneInput/__attribute__((weak)) &/}' $FUZZER/repo/examples/aflpp_driver/aflpp_driver.c
+cat >> $FUZZER/repo/examples/aflpp_driver/aflpp_driver.c << EOF
+__attribute__((weak))
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{
+  // assert(0 && "LLVMFuzzerTestOneInput should not be implemented in afl_driver");
+  return 0;
+}
+EOF
