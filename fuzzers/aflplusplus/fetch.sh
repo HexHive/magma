bc16a32e033f989b0db15fa6569989c72db6dbbd
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-08-10 19:42:40 +0200

    Weak-ened AFL driver symbols for config/cmake

--- fuzzers/aflplusplus/fetch.sh
+++ fuzzers/aflplusplus/fetch.sh
@@ -7,3 +7,15 @@ set -e
 ##
 
 git clone --depth 1 https://github.com/AFLplusplus/AFLplusplus "$FUZZER/repo"
+
+# Fix: CMake-based build systems fail with duplicate (of main) or undefined references (of LLVMFuzzerTestOneInput)
+sed -i '{s/^int main/__attribute__((weak)) &/}' $FUZZER/repo/examples/aflpp_driver/aflpp_driver.c
+sed -i '{s/^int LLVMFuzzerTestOneInput/__attribute__((weak)) &/}' $FUZZER/repo/examples/aflpp_driver/aflpp_driver.c
+cat >> $FUZZER/repo/examples/aflpp_driver/aflpp_driver.c << EOF
+__attribute__((weak))
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{
+  // assert(0 && "LLVMFuzzerTestOneInput should not be implemented in afl_driver");
+  return 0;
+}
+EOF
