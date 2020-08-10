46a1225d6957605410990a472ad7f4430d0d2a01
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-08-10 19:46:12 +0200

    Fixed magma/build.sh to use default linker

--- magma/build.sh
+++ magma/build.sh
@@ -16,5 +16,5 @@ $CC $CFLAGS -D"MAGMA_STORAGE=\"$MAGMA_STORAGE\"" -c "$MAGMA/src/canary.c" \
 $CC $CFLAGS -D"MAGMA_STORAGE=\"$MAGMA_STORAGE\"" -c "$MAGMA/src/storage.c" \
     -fPIC -I "$MAGMA/src/" -o "$OUT/storage.o" $LDFLAGS
 
-ld -r "$OUT/canary.o" "$OUT/storage.o" -o "$OUT/magma.o"
+$LD -r "$OUT/canary.o" "$OUT/storage.o" -o "$OUT/magma.o"
 rm "$OUT/canary.o" "$OUT/storage.o"
