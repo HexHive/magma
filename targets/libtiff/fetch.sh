f7f7e5b557006d6b4771fe73d86f837534bc65fa
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-08-27 01:56:10 +0200

    Fixed libtiff::jbigkit build not using env vars

--- targets/libtiff/fetch.sh
+++ targets/libtiff/fetch.sh
@@ -17,5 +17,24 @@ git clone https://github.com/libjpeg-turbo/libjpeg-turbo \
 git clone https://www.cl.cam.ac.uk/~mgk25/git/jbigkit \
     "$TARGET/jbigkit"
 
+# Uncomment default CC and CFLAGS from the build of jbigkit
+patch -p1 -d "$TARGET/jbigkit" << EOF
+--- a/Makefile
++++ b/Makefile
+@@ -1,10 +1,10 @@
+ # Unix makefile for JBIG-KIT
+ 
+ # Select an ANSI/ISO C compiler here, GNU gcc is recommended
+-CC = gcc
++# CC = gcc
+ 
+ # Options for the compiler: A high optimization level is suggested
+-CFLAGS = -O2 -W -Wno-unused-result
++# CFLAGS = -O2 -W -Wno-unused-result
+ # CFLAGS = -O -g -W -Wall -Wno-unused-result -ansi -pedantic # -DDEBUG
+ 
+ export CC CFLAGS
+EOF
+
 cp "$TARGET/src/tiff_read_rgba_fuzzer.cc" \
     "$TARGET/repo/contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc"
