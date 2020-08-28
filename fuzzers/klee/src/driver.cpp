d539946f07947bba5270dcc17d6e66075a6f7e15
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-08-28 21:26:21 +0200

    Replaced KLEE driver to read from stdin

--- fuzzers/klee/src/driver.cpp
+++ fuzzers/klee/src/driver.cpp
@@ -16,6 +16,13 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+// Input buffer.
+static const size_t kMaxAflInputSize = 1 << 20;
+static uint8_t AflInputBuf[kMaxAflInputSize];
 
 extern "C" {
 
@@ -27,18 +34,30 @@ __attribute__((weak))
 int main(int argc, char **argv) {
   if (LLVMFuzzerInitialize)
     LLVMFuzzerInitialize(&argc, &argv);
-  for (int i = 1; i < argc; i++) {
-    FILE *f = fopen(argv[i], "r");
-    assert(f);
-    fseek(f, 0, SEEK_END);
-    size_t len = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    unsigned char *buf = (unsigned char*)malloc(len);
-    size_t n_read = fread(buf, 1, len, f);
-    fclose(f);
-    assert(n_read == len);
-    LLVMFuzzerTestOneInput(buf, len);
-    free(buf);
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      FILE *f = fopen(argv[i], "r");
+      assert(f);
+      fseek(f, 0, SEEK_END);
+      size_t len = ftell(f);
+      fseek(f, 0, SEEK_SET);
+      unsigned char *buf = (unsigned char*)malloc(len);
+      size_t n_read = fread(buf, 1, len, f);
+      fclose(f);
+      assert(n_read == len);
+      LLVMFuzzerTestOneInput(buf, len);
+      free(buf);
+    }
+  } else {
+    ssize_t n_read = read(0, AflInputBuf, kMaxAflInputSize);
+    if (n_read > 0) {
+      // Copy AflInputBuf into a separate buffer to let asan find buffer
+      // overflows. Don't use unique_ptr/etc to avoid extra dependencies.
+      uint8_t *copy = new uint8_t[n_read];
+      memcpy(copy, AflInputBuf, n_read);
+      LLVMFuzzerTestOneInput(copy, n_read);
+      delete[] copy;
+    }
   }
   return 0;
 }
