47a424c780594d2551f9e9126ceae47475814a4b
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-08-28 19:20:44 +0200

    Added SymCC analysis configuration

---
+++ fuzzers/symcc_analysis/build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+##
+
+if [ ! -d "$FUZZER/symcc" ] || [ ! -d "$FUZZER/z3" ] || \
+   [ ! -d "$FUZZER/llvm" ]; then
+    echo "fetch.sh must be executed first."
+    exit 1
+fi
+
+# build Z3
+(
+    cd "$FUZZER/z3"
+    mkdir build install cmake_conf
+    cd build
+    CXX=clang++ CC=clang cmake ../ \
+        -DCMAKE_INSTALL_PREFIX="$FUZZER/z3/install" \
+        -DCMAKE_INSTALL_Z3_CMAKE_PACKAGE_DIR="$FUZZER/z3/cmake_conf"
+    make -j $(nproc)
+    make install
+)
+
+# build SymCC
+(
+    cd "$FUZZER/symcc"
+
+    mkdir build
+    pushd build
+    cmake -G Ninja ../ \
+        -DQSYM_BACKEND=OFF \
+        -DZ3_DIR="$FUZZER/z3/cmake_conf"
+    ninja check
+    popd
+
+    pushd util/symcc_fuzzing_helper
+    cargo build --release
+    popd
+)
+
+# build libc++
+(
+    cd "$FUZZER/llvm"
+    mkdir libcxx_symcc libcxx_symcc_install
+    cd libcxx_symcc
+    export SYMCC_REGULAR_LIBCXX=yes
+    export SYMCC_NO_SYMBOLIC_INPUT=yes
+    cmake -G Ninja ../llvm \
+        -DLLVM_ENABLE_PROJECTS="libcxx;libcxxabi" \
+        -DLLVM_TARGETS_TO_BUILD="X86" \
+        -DLLVM_DISTRIBUTION_COMPONENTS="cxx;cxxabi;cxx-headers" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FUZZER/llvm/libcxx_symcc_install" \
+        -DCMAKE_C_COMPILER="$FUZZER/symcc/build/symcc" \
+        -DCMAKE_CXX_COMPILER="$FUZZER/symcc/build/sym++"
+    ninja distribution
+    ninja install-distribution
+)
+
+# compile driver.cpp
+export SYMCC_LIBCXX_PATH="$FUZZER/llvm/libcxx_symcc_install"
+"$FUZZER/symcc/build/sym++" $CXXFLAGS -std=c++11 -c -fPIC \
+    "$FUZZER/src/driver.cpp" -o "$OUT/driver.o"
---
+++ fuzzers/symcc_analysis/fetch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+##
+
+git clone --no-checkout https://github.com/eurecom-s3/symcc.git "$FUZZER/symcc"
+git -C "$FUZZER/symcc" checkout 1cc757dfbc9ac7e26ddcadd48d38c30c2c540116
+git -C "$FUZZER/symcc" submodule init
+git -C "$FUZZER/symcc" submodule update
+
+git clone --no-checkout https://github.com/Z3Prover/z3.git "$FUZZER/z3"
+git -C "$FUZZER/z3" checkout 78b88f761ca21f0287eb6563092b706d15c7b71b
+
+git clone --depth 1 -b release/11.x \
+    https://github.com/llvm/llvm-project.git "$FUZZER/llvm"
