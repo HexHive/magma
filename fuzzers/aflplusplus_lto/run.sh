2e5367d548f8a4bda9121dfe066afd4e590a286e
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2020-09-01 07:09:46 +0200

    Disabled deferred forkserver in AFL++ driver
    This was causing issues with non-persistent programs that linked against
    the AFL++ driver. The driver was indicating to AFL++ that it performs
    manual initialization of the forkserver, but the non-persistent tools
    were not making that function call, and the forkserver handshake fails.

--- fuzzers/aflplusplus_lto/run.sh
+++ fuzzers/aflplusplus_lto/run.sh
@@ -18,6 +18,7 @@ export AFL_SKIP_CPUFREQ=1
 export AFL_NO_AFFINITY=1
 export AFL_NO_UI=1
 export AFL_MAP_SIZE=256000
+export AFL_DRIVER_DONT_DEFER=1
 
 "$FUZZER/repo/afl-fuzz" -i "$TARGET/corpus/$PROGRAM" -o "$SHARED/findings" \
     "${flag_cmplog[@]}" -d \
