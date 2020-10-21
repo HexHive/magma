dc69fe9bf4790be4e3f152e6d1ee18633f1b84b6
Adrian Herrera <adrian.herrera02@gmail.com>
2020-10-21 00:19:47 +0000

    added InsTrim fuzzer

---
+++ fuzzers/instrim/instrument.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+# - env TARGET: path to target work dir
+# - env MAGMA: path to Magma support files
+# - env OUT: path to directory where artifacts are stored
+# - env CFLAGS and CXXFLAGS must be set to link against Magma instrumentation
+##
+
+export CC="$FUZZER/afl/afl-clang-fast"
+export CXX="$FUZZER/afl/afl-clang-fast++"
+export AS="$FUZZER/afl/afl-as"
+
+export LIBS="$LIBS -l:afl_driver.o -lstdc++"
+
+export INSTRIM_LIB="$FUZZER/instrim/build/libLLVMInsTrim.so"
+export MARKSET="1"
+
+"$MAGMA/build.sh"
+"$TARGET/build.sh"
+
+# NOTE: We pass $OUT directly to the target build.sh script, since the artifact
+#       itself is the fuzz target. In the case of Angora, we might need to
+#       replace $OUT by $OUT/fast and $OUT/track, for instance.
