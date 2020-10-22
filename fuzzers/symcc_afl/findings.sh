b56a9ed241b5c30380bedf760bd6ca4ddd4de583
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2020-10-22 09:38:41 +0200

    Fixed SymCC-AFL findings script

--- fuzzers/symcc_afl/findings.sh
+++ fuzzers/symcc_afl/findings.sh
@@ -5,10 +5,10 @@
 # - env SHARED: path to directory shared with host (to store results)
 ##
 
-CRASH_DIR="$SHARED/findings/crashes"
+CRASH_DIRS=("$SHARED/findings/afl-master/crashes" "$SHARED/findings/symcc/crashes")
 
-if [ ! -d "$CRASH_DIR" ]; then
+if [ ! -d "${CRASH_DIRS[0]}" ] && [ ! -d "${CRASH_DIRS[1]}" ]; then
     exit 1
 fi
 
-find "$CRASH_DIR" -type f -name 'id:*'
+find ${CRASH_DIRS[@]} -type f -name 'id:*'
