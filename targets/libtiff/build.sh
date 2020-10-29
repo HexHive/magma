e39060bb3399ecebf98d006a0dc0bb26d7349a79
Adrian Herrera <adrian.herrera02@gmail.com>
2020-10-29 10:37:26 +0100

    libtiff: use system dependencies

--- targets/libtiff/build.sh
+++ targets/libtiff/build.sh
@@ -18,35 +18,6 @@ rm -rf "$WORK"
 mkdir -p "$WORK"
 mkdir -p "$WORK/lib" "$WORK/include"
 
-# build zlib
-pushd "$TARGET/zlib"
-./configure --static --prefix="$WORK"
-make -j$(nproc) clean
-make -j$(nproc) CFLAGS="$CFLAGS -fPIC"
-make install
-popd
-
-# Build libjpeg-turbo
-pushd "$TARGET/libjpeg-turbo"
-EXTRA=""
-test -n "$AR" && EXTRA="$EXTRA -DCMAKE_AR=$AR"
-test -n "$RANLIB" && EXTRA="$EXTRA -DCMAKE_RANLIB=$RANLIB"
-cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DENABLE_STATIC=on -DENABLE_SHARED=off $EXTRA
-make -j$(nproc) clean
-make -j$(nproc)
-make install
-EXTRA=""
-popd
-
-# Build libjbig
-pushd "$TARGET/jbigkit"
-make clean
-make lib
-
-cp "$TARGET"/jbigkit/libjbig/*.a "$WORK/lib/"
-cp "$TARGET"/jbigkit/libjbig/*.h "$WORK/include/"
-popd
-
 cd "$TARGET/repo"
 ./autogen.sh
 ./configure --disable-shared --prefix="$WORK"
@@ -57,6 +28,5 @@ make install
 cp "$WORK/bin/tiffcp" "$OUT/"
 $CXX $CXXFLAGS -std=c++11 -I$WORK/include \
     contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc -o $OUT/tiff_read_rgba_fuzzer \
-    $WORK/lib/libtiffxx.a $WORK/lib/libtiff.a $WORK/lib/libz.a $WORK/lib/libjpeg.a \
-    $WORK/lib/libjbig.a $WORK/lib/libjbig85.a -Wl,-Bstatic -llzma -Wl,-Bdynamic \
+    $WORK/lib/libtiffxx.a $WORK/lib/libtiff.a -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic \
     $LDFLAGS $LIBS
