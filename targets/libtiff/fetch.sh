f4727469b284434830ef9c3615bbe18eb26b6dc8
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2020-10-29 15:18:12 +0100

    Anchored targets to latest versions

--- targets/libtiff/fetch.sh
+++ targets/libtiff/fetch.sh
@@ -5,9 +5,9 @@
 # - env TARGET: path to target work dir
 ##
 
-git clone https://gitlab.com/libtiff/libtiff \
-    --depth 1 --branch master \
+git clone --no-checkout https://gitlab.com/libtiff/libtiff.git \
     "$TARGET/repo"
+git -C "$TARGET/repo" checkout 2e822691d750c01cec5b5cc4ee73567a204ab2a3
 
 cp "$TARGET/src/tiff_read_rgba_fuzzer.cc" \
     "$TARGET/repo/contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc"
--- targets/poppler/fetch.sh
+++ targets/poppler/fetch.sh
@@ -5,9 +5,10 @@
 # - env TARGET: path to target work dir
 ##
 
-git clone https://anongit.freedesktop.org/git/poppler/poppler.git \
-    --depth 1 --branch master \
+git clone --no-checkout https://gitlab.freedesktop.org/poppler/poppler.git \
     "$TARGET/repo"
-git clone git://git.sv.nongnu.org/freetype/freetype2.git \
-    --depth 1 --branch master \
+git -C "$TARGET/repo" checkout 2706eca3ad3af99fa6551b9d6fcdc69eb0a0aa4e
+
+git clone --no-checkout git://git.sv.nongnu.org/freetype/freetype2.git \
     "$TARGET/freetype2"
+git -C "$TARGET/freetype2" checkout 804e625def2cfb64ef2f4c8877cd3fa11e86e208
