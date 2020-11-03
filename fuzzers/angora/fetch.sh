9541b7c621a45666e884f90181d4aeade989df26
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2020-11-03 20:30:12 +0100

    ok

--- fuzzers/angora/fetch.sh
+++ fuzzers/angora/fetch.sh
@@ -10,4 +10,5 @@ git clone https://github.com/adrianherrera/Angora.git \
 	--branch improvement/angora-showmap \
 	"$FUZZER/repo"
 
-cp "$FUZZER/src/angora_driver.c" "$FUZZER/repo/angora_driver.c"
+cp "$FUZZER/src/angora_driver.c" "$FUZZER/repo"
+cp "$FUZZER/src/afl-cmin" "$FUZZER/repo"
