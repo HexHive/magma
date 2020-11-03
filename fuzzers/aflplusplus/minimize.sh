bdf0d878205c8ccc9260086f25d561ae4b29d221
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2020-11-03 19:30:55 +0100

    feed mi ze fixez

--- fuzzers/aflplusplus/minimize.sh
+++ fuzzers/aflplusplus/minimize.sh
@@ -13,7 +13,7 @@
 
 export AFL_PATH="$FUZZER/repo"
 "$FUZZER/repo/afl-cmin" -m 100 -i "$CORPUS_IN" -o "$CORPUS_OUT/tmp" \
-    -- "$OUT/$PROGRAM" $ARGS 2>&1
+    -- "$OUT/afl/$PROGRAM" $ARGS 2>&1
 
 find "$CORPUS_OUT/tmp" -maxdepth 1 -type f -exec mv {} "$CORPUS_OUT" \;
 rm -rf "$CORPUS_OUT/tmp"
