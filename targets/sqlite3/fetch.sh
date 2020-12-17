5d81041f3d9154ade9f0fe889fb65f3f77329571
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2020-12-17 17:44:47 +0100

    Anchored sqlite3 to proper working reference

--- targets/sqlite3/fetch.sh
+++ targets/sqlite3/fetch.sh
@@ -5,6 +5,7 @@
 # - env TARGET: path to target work dir
 ##
 
-curl https://www.sqlite.org/src/tarball/sqlite.tar.gz -o "$OUT/sqlite.tar.gz" && \
+curl "https://www.sqlite.org/src/tarball/sqlite.tar.gz?r=16e281ed6219cc22" \
+  -o "$OUT/sqlite.tar.gz" && \
 mkdir -p "$TARGET/repo" && \
 tar -C "$TARGET/repo" --strip-components=1 -xzf "$OUT/sqlite.tar.gz"
