1e9d2399d1d576ea160912f0ec680d6ab791f4b4
Adrian Herrera <adrian.herrera02@gmail.com>
2021-02-10 10:14:09 +1100

    benchd: save raw time-to-bug values
    Makes performing the log-rank test for statistical similarity easier.
    Also save times in seconds (rather than hours)

--- tools/benchd/survival_analysis.py
+++ tools/benchd/survival_analysis.py
@@ -11,9 +11,10 @@ Author: Adrian Herrera
 
 from argparse import ArgumentParser, Namespace
 from collections import defaultdict
+from itertools import product
 from math import sqrt
 from pathlib import Path
-from typing import Tuple
+from typing import List, Tuple
 import json
 import warnings
 
@@ -22,6 +23,7 @@ from lifelines.utils import restricted_mean_survival_time as rmst
 import pandas as pd
 
 
+METRICS = ('reached', 'triggered')
 ddr = lambda: defaultdict(ddr)
 
 
@@ -32,6 +34,8 @@ def parse_args() -> Namespace:
                         help='Number of trials')
     parser.add_argument('-t', '--trial-length', type=int, required=True,
                         help='Length of an individual trial (in seconds)')
+    parser.add_argument('-r', '--raw-data', action='store_true',
+                        help='Store the raw run data')
     parser.add_argument('json', type=Path,
                         help='Magma-generated JSON file (containing bug data)')
     return parser.parse_args()
@@ -59,18 +63,17 @@ def get_time_to_bug(data: dict, num_trials: int) -> dict:
                     )
 
 
-def calc_survival(data: dict, trial_len: int) -> Tuple[float, float]:
+def calc_survival(data: List[int], trial_len: int) -> Tuple[float, float]:
     """Do the survival analysis."""
     df = pd.DataFrame(data)
-    T = df.fillna(trial_len) / 60 / 60
+    T = df.fillna(trial_len)
     E = df.notnull()
 
     kmf = KaplanMeierFitter()
     kmf.fit(T, E)
 
     # Compute the restricted mean survival time and 95% confidence interval
-    surv_time_mean, surv_time_var = rmst(kmf, t=trial_len / 60 / 60,
-                                         return_variance=True)
+    surv_time_mean, surv_time_var = rmst(kmf, t=trial_len, return_variance=True)
     surv_time_var = abs(surv_time_var)
     surv_time_ci = 1.96 * (sqrt(surv_time_var) /
                            sqrt(len(kmf.survival_function_)))
@@ -81,32 +84,46 @@ def calc_survival(data: dict, trial_len: int) -> Tuple[float, float]:
 def main():
     """The main function."""
     args = parse_args()
+    num_trials = args.num_trials
 
     # Ignore warnings
     warnings.simplefilter('ignore')
 
-    survival_times = dict(target=[],
-                          program=[],
-                          bug=[],
-                          fuzzer=[],
-                          survival_time_reached=[],
-                          survival_ci_reached=[],
-                          survival_time_triggered=[],
-                          survival_ci_triggered=[])
+    surv_data = dict(target=[],
+                         program=[],
+                         bug=[],
+                         fuzzer=[],
+                         survival_time_reached=[],
+                         survival_ci_reached=[],
+                         survival_time_triggered=[],
+                         survival_ci_triggered=[])
+
+    # Create fields for storing the raw times (if required)
+    if args.raw_data:
+        for run in range(0, num_trials):
+            surv_data[f'reached_{run}'] = []
+            surv_data[f'triggered_{run}'] = []
 
     # Read Magma JSON data
     with args.json.open() as inf:
         json_data = json.load(inf).get('results', {})
 
-    # Do survival analysis on 'triggered' results
-    for ttb in get_time_to_bug(json_data, args.num_trials):
+    # Do survival analysis on the time-to-bug results
+    for ttb in get_time_to_bug(json_data, num_trials):
         # Save table data
-        survival_times['target'].append(ttb['target'])
-        survival_times['program'].append(ttb['program'])
-        survival_times['bug'].append(ttb['bug'])
-        survival_times['fuzzer'].append(ttb['fuzzer'])
+        surv_data['target'].append(ttb['target'])
+        surv_data['program'].append(ttb['program'])
+        surv_data['bug'].append(ttb['bug'])
+        surv_data['fuzzer'].append(ttb['fuzzer'])
 
-        for metric in ('reached', 'triggered'):
+        # Save raw times (if required)
+        if args.raw_data:
+            for metric, run in product(METRICS, range(0, num_trials)):
+                time = ttb.get(metric, [None] * num_trials)[run]
+                surv_data[f'{metric}_{run}'].append(time)
+
+        # Do survival analysis
+        for metric in METRICS:
             if metric not in ttb:
                 surv_time_mean = None
                 surv_time_ci = None
@@ -114,12 +131,12 @@ def main():
                 surv_time_mean, surv_time_ci = calc_survival(ttb[metric],
                                                              args.trial_length)
 
-            survival_times[f'survival_time_{metric}'].append(surv_time_mean)
-            survival_times[f'survival_ci_{metric}'].append(surv_time_ci)
+            surv_data[f'survival_time_{metric}'].append(surv_time_mean)
+            surv_data[f'survival_ci_{metric}'].append(surv_time_ci)
 
     # Write to CSV
     order = ['bug', 'program', 'target', 'fuzzer']
-    print(pd.DataFrame.from_dict(survival_times).sort_values(by=order).to_csv(index=False))
+    print(pd.DataFrame.from_dict(surv_data).sort_values(by=order).to_csv(index=False))
 
 
 if __name__ == '__main__':
