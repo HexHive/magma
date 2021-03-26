1f4f4b28ecff2492c59f2b645addfd2bbad91944
Wei-Cheng Wu <wwu@isi.edu>
2021-03-26 15:21:27 -0700

    Update driller scripts

---
+++ fuzzers/driller/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xe
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+# - env TARGET: path to target work dir
+# - env OUT: path to directory where artifacts are stored
+# - env SHARED: path to directory shared with host (to store results)
+# - env PROGRAM: name of program to run (should be found in $OUT)
+# - env ARGS: extra arguments to pass to the program
+# - env FUZZARGS: extra arguments to pass to the fuzzer
+##
+
+mkdir -p "$SHARED/findings"
+
+export AFL_SKIP_CPUFREQ=1
+export AFL_NO_AFFINITY=1
+cd $OUT
+shellphuzz --memory 100M -s "$TARGET/corpus/$PROGRAM" -w "$SHARED/findings" \
+    -c 1 -d 1 "$OUT/$PROGRAM"  2>&1
