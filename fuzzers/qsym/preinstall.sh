97be1cdeb5b53e065429f1999b6b0c3b2d06696f
sojhal <sojhal@gmail.com>
2021-04-14 23:26:59 -0700

    added afl building in preinstall for qsym. added qsym code with xdist commented out.

--- fuzzers/qsym/preinstall.sh
+++ fuzzers/qsym/preinstall.sh
@@ -5,10 +5,10 @@ apt-get update && \
     apt-get install -y make build-essential git wget
 
 # Install QSYM fuzzer
-git clone https://github.com/sslab-gatech/qsym.git
+#git clone https://github.com/sslab-gatech/qsym.git
 pushd qsym
 # disable ptrace_scope for PIN
-echo 0|sudo tee /proc/sys/kernel/yama/ptrace_scope
+#echo 0|sudo tee /proc/sys/kernel/yama/ptrace_scope
 
 # install z3 and system deps
 ./setup.sh
@@ -16,6 +16,11 @@ echo 0|sudo tee /proc/sys/kernel/yama/ptrace_scope
 # install using virtual env
 # virtualenv venv
 # source venv/bin/activate
-$ pip install .
+pip install .
 popd
 
+# Install AFL
+git clone https://github.com/google/AFL.git
+pushd AFL
+make
+popd
