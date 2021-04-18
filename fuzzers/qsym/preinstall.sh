8468d084bb98f4f9b26795fe52b77907adab9e76
sojhal <sojhal@gmail.com>
2021-04-18 00:05:26 -0700

    experimenting with installing afl

--- fuzzers/qsym/preinstall.sh
+++ fuzzers/qsym/preinstall.sh
@@ -5,23 +5,26 @@ apt-get update && \
     apt-get install -y make build-essential git wget
 
 # Install QSYM fuzzer
-git clone https://github.com/sojhal/qsym.git
+#git clone https://github.com/sojhal/qsym.git
 
-pushd qsym
+#pushd qsym
 # disable ptrace_scope for PIN
 #echo 0|sudo tee /proc/sys/kernel/yama/ptrace_scope
 
 # install z3 and system deps
-./setup.sh
+#./setup.sh
 
 # install using virtual env
 # virtualenv venv
 # source venv/bin/activate
-pip install .
-popd
+#pip install .
+#popd
 
 # Install AFL
 git clone https://github.com/google/AFL.git
 pushd AFL
-make
+sudo ln -s /usr/bin/make /usr/bin/gmake
+sudo apt install clang -y
+export LLVM_CONFIG=/usr/bin/llvm-config-3.8
+AFL_NO_X86=1 gmake && gmake -C llvm_mode
 popd
