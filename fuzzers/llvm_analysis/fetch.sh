23857ac45addc17eff65363857659a7f14075b4c
Adrian Herrera <adrian.herrera02@gmail.com>
2021-06-30 07:36:06 +0000

    llvm_analysis: simplified go install

--- fuzzers/llvm_analysis/fetch.sh
+++ fuzzers/llvm_analysis/fetch.sh
@@ -6,16 +6,7 @@ set -e
 # - env FUZZER: path to fuzzer work dir
 ##
 
-GO_VERSION="1.15"
-GO_OS="linux"
-GO_ARCH="amd64"
 
-mkdir -p "$FUZZER/repo/goroot" "$FUZZER/repo/gopath"
-curl -L "https://golang.org/dl/go${GO_VERSION}.${GO_OS}-${GO_ARCH}.tar.gz" | \
-    tar xzf - -C "$FUZZER/repo/goroot" --strip-components=1
-
-export GOROOT="$FUZZER/repo/goroot"
-export GOPATH="$FUZZER/repo/gopath"
-export PATH=$PATH:"$GOROOT/bin":"$GOPATH/bin"
-
+export GOPATH="$FUZZER/repo/go"
+mkdir -p $GOPATH
 go get github.com/SRI-CSL/gllvm/cmd/...
