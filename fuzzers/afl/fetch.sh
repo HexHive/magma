9ca1c40ac48f48914c039b05edfc0d73ad760af0
Adrian Herrera <adrian.herrera02@gmail.com>
2021-07-12 01:50:58 +0000

    afl: bugfix
    Incorrectly-placed `$` :)

--- fuzzers/afl/fetch.sh
+++ fuzzers/afl/fetch.sh
@@ -7,7 +7,7 @@ set -e
 ##
 
 git clone --no-checkout https://github.com/google/AFL.git "$FUZZER/repo"
-git -C $"FUZZER/repo" checkout 61037103ae3722c8060ff7082994836a794f978e
+git -C "$FUZZER/repo" checkout 61037103ae3722c8060ff7082994836a794f978e
 #wget -O "$FUZZER/repo/afl_driver.cpp" \
 #    "https://cs.chromium.org/codesearch/f/chromium/src/third_party/libFuzzer/src/afl/afl_driver.cpp"
 cp "$FUZZER/src/afl_driver.cpp" "$FUZZER/repo/afl_driver.cpp"
