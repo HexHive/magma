36fd063a61c1e39b5301c00c8c1690c03d2429c6
Adrian Herrera <adrian.herrera02@gmail.com>
2021-08-15 17:13:21 +1000

    symcc_analysis: updated CMake
    Fixes issue #84

--- fuzzers/symcc_analysis/fetch.sh
+++ fuzzers/symcc_analysis/fetch.sh
@@ -6,13 +6,11 @@ set -e
 # - env FUZZER: path to fuzzer work dir
 ##
 
-git clone --no-checkout https://github.com/eurecom-s3/symcc.git "$FUZZER/symcc"
-git -C "$FUZZER/symcc" checkout 1cc757dfbc9ac7e26ddcadd48d38c30c2c540116
+git clone --depth 1 https://github.com/eurecom-s3/symcc.git "$FUZZER/symcc"
 git -C "$FUZZER/symcc" submodule init
 git -C "$FUZZER/symcc" submodule update
 
-git clone --no-checkout https://github.com/Z3Prover/z3.git "$FUZZER/z3"
-git -C "$FUZZER/z3" checkout 78b88f761ca21f0287eb6563092b706d15c7b71b
+git clone --depth 1 https://github.com/Z3Prover/z3.git "$FUZZER/z3"
 
 git clone --depth 1 -b release/11.x \
     https://github.com/llvm/llvm-project.git "$FUZZER/llvm"
